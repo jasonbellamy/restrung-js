4a3d3139eabb1c1def5d544be992188e1df0371c
Jason Bellamy <j@sonbellamy.com>
2015-09-11 22:21:26 -0400

    Fix issue with spaceCase not matching all cases
    spaceCase was only inserting a space after the first uppercase character match.
    This was caused by not including a \g flag in the regular expression.

--- test/space-case.js
+++ test/space-case.js
@@ -3,11 +3,11 @@ import spaceCase from '../src/space-case.js';
 
 describe('#spaceCase', () => {
   it('should spaceCase a string', () => {
-    assert.equal(spaceCase('camelCased'), 'camel Cased');
-    assert.equal(spaceCase('spinal-cased'), 'spinal cased');
-    assert.equal(spaceCase('snake_cased'), 'snake cased');
-    assert.equal(spaceCase('dot.notation'), 'dot notation');
-    assert.equal(spaceCase('space cased'), 'space cased');
+    assert.equal(spaceCase('camelCasedString'), 'camel Cased String');
+    assert.equal(spaceCase('spinal-cased-string'), 'spinal cased string');
+    assert.equal(spaceCase('snake_cased_string'), 'snake cased string');
+    assert.equal(spaceCase('dot.notation.string'), 'dot notation string');
+    assert.equal(spaceCase('space cased string'), 'space cased string');
     assert.equal(spaceCase('camelSpinal-snake_dot.cased'), 'camel Spinal snake dot cased');
   });
 });
