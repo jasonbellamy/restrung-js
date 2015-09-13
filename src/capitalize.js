376c6f66888ac2f9c322756d6d59c1abc982dc0f
Jason Bellamy <j@sonbellamy.com>
2015-09-13 19:23:17 -0400

    Simplify function documentation

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,13 +1,7 @@
 import R from 'ramda';
 import transformHead from './util/transform-head.js';
 
-/**
- * Uppercase the first letter of a string.
- *
- * @sig a -> a
- * @param {String} s
- * @return {String}
- */
+// a -> a
 const capitalize = transformHead(R.toUpper);
 
 export default capitalize;
