b88c1a527a984583a9747a466275c1c1d639282a
Jason Bellamy <j@sonbellamy.com>
2015-09-22 15:00:36 -0400

    Refactor truncate function to use custom compose/curry function

--- src/truncate.js
+++ src/truncate.js
@@ -1,11 +1,15 @@
 import R from 'ramda';
+import curry from './util/curry.js';
+import compose from './util/compose.js';
 
 // a -> b -> b
-const truncate = R.compose(
-  R.join(''),
-  R.append('...'),
-  R.head,
-  R.splitEvery
-);
+const truncate = curry(function(a, b) {
+  return compose(
+    R.join(''),
+    R.append('...'),
+    R.head,
+    R.splitEvery(a)
+  )(b);
+});
 
 export default truncate;
