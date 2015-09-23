90aa1d61389f8a681e6fcf18fb3a576e0907a249
Jason Bellamy <j@sonbellamy.com>
2015-09-22 22:25:37 -0400

    Refactor existing functions to use custom split function

--- src/pascal-case.js
+++ src/pascal-case.js
@@ -1,6 +1,7 @@
 import R from 'ramda';
 import compose from './util/compose.js';
 import join from './util/join.js';
+import split from './util/split.js';
 import lowerCase from './util/lower-case.js';
 import capitalize from './capitalize.js';
 import spaceCase from './space-case.js';
@@ -9,7 +10,7 @@ import spaceCase from './space-case.js';
 const pascalCase = compose(
   join(''),
   R.map(compose(capitalize, lowerCase)),
-  R.split(' '),
+  split(' '),
   spaceCase
 );
 
