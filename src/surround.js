6e503e11656b71afcf96981c0900f34a10f29f30
Jason Bellamy <j@sonbellamy.com>
2015-09-23 22:04:50 -0400

    Refactor surround function to use custom prepend function

--- src/surround.js
+++ src/surround.js
@@ -2,13 +2,14 @@ import R from 'ramda';
 import curry from './util/curry.js';
 import compose from './util/compose.js';
 import join from './util/join.js';
+import prepend from './util/prepend.js';
 
 // a -> a -> a
 const surround = curry((a, b) => {
   return compose(
     join(''),
     R.append(a),
-    R.prepend(a)
+    prepend(a)
   )(b);
 });
 
