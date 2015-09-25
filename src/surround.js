13d438558af399144d1f8da89a2d4f567c4b771d
Jason Bellamy <j@sonbellamy.com>
2015-09-25 18:36:23 -0400

    Refactor surround/truncate function to use custom append function

--- src/surround.js
+++ src/surround.js
@@ -1,14 +1,13 @@
-import R from 'ramda';
 import curry from './util/curry.js';
 import compose from './util/compose.js';
 import join from './util/join.js';
 import prepend from './util/prepend.js';
+import append from './util/append.js';
 
 // a -> a -> a
 const surround = curry((a, b) => {
   return compose(
-    join(''),
-    R.append(a),
+    append(a),
     prepend(a)
   )(b);
 });
