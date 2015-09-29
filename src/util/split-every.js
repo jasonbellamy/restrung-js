e745ce272b3e31ff91d6cc412db7d6bb08e19600
Jason Bellamy <j@sonbellamy.com>
2015-09-28 22:43:39 -0400

    Add tests for splitEvery function

---
+++ src/util/split-every.js
@@ -0,0 +1,6 @@
+import curry from './curry.js';
+
+// a -> b -> b
+const splitEvery = null;
+
+export default splitEvery;
---
+++ test/util/split-every.js
@@ -0,0 +1,19 @@
+import {assert} from 'chai';
+import splitEvery from '../../src/util/split-every.js';
+
+describe('#splitEvery', () => {
+  it('should split a collection into slices of the specified length', function() {
+    assert.deepEqual(R.splitEvery(1, [1, 2, 3, 4]), [[1], [2], [3], [4]]);
+    assert.deepEqual(R.splitEvery(2, [1, 2, 3, 4]), [[1, 2], [3, 4]]);
+    assert.deepEqual(R.splitEvery(3, [1, 2, 3, 4]), [[1, 2, 3], [4]]);
+    assert.deepEqual(R.splitEvery(4, [1, 2, 3, 4]), [[1, 2, 3, 4]]);
+    assert.deepEqual(R.splitEvery(5, [1, 2, 3, 4]), [[1, 2, 3, 4]]);
+    assert.deepEqual(R.splitEvery(3, []), []);
+    assert.deepEqual(R.splitEvery(1, 'abcd'), ['a', 'b', 'c', 'd']);
+    assert.deepEqual(R.splitEvery(2, 'abcd'), ['ab', 'cd']);
+    assert.deepEqual(R.splitEvery(3, 'abcd'), ['abc', 'd']);
+    assert.deepEqual(R.splitEvery(4, 'abcd'), ['abcd']);
+    assert.deepEqual(R.splitEvery(5, 'abcd'), ['abcd']);
+    assert.deepEqual(R.splitEvery(3, ''), []);
+  });
+});
