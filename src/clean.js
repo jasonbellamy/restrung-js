2a1addb057d1eae2ed297303ce8ca480bd0583a0
Jason Bellamy <j@sonbellamy.com>
2015-10-01 17:32:19 -0400

    Refactor clean function to use custom replace function

--- src/clean.js
+++ src/clean.js
@@ -1,11 +1,11 @@
-import R from 'ramda';
 import compose from './util/compose.js';
+import replace from './util/replace.js';
 import trim from './trim.js';
 
 // a -> a
 const clean = compose(
   trim,
-  R.replace(/\s{2}/, '')
+  replace(/\s{2}/, '')
 );
 
 export default clean
