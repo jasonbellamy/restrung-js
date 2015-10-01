c0b803ba3f11b358771c535425ad74250b497110
Jason Bellamy <j@sonbellamy.com>
2015-10-01 17:32:58 -0400

    Refactor transformHead test to use custom functions

--- test/util/transform-head.js
+++ test/util/transform-head.js
@@ -1,10 +1,11 @@
 import {assert} from 'chai';
-import R from 'ramda';
+import upperCase from '../../src/util/upper-case.js';
+import lowerCase from '../../src/util/lower-case.js';
 import transformHead from '../../src/util/transform-head.js';
 
 describe('#transformHead', () => {
   it('should transform the head of a string', () => {
-    assert.equal(transformHead(R.toUpper, 'hello'), 'Hello');
-    assert.equal(transformHead(R.toLower, 'Hello'), 'hello');
+    assert.equal(transformHead(upperCase, 'hello'), 'Hello');
+    assert.equal(transformHead(lowerCase, 'Hello'), 'hello');
   });
 });
